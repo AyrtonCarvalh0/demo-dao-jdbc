d21814d5fd80aadc156b3fc23c90974e7a503c43
Ayrton Carvalho <cayrton021@gmail.com>
2025-12-15 14:42:11 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department dep = new Department(1 , "Books");
 		
 		System.out.println(dep);
+		
+		Seller sell = new Seller(21 , "Drake" , "Drake@gmail.com" , new Date() , 2500.0 , dep);
+		
+		System.out.println(sell);
 
 	}
 
